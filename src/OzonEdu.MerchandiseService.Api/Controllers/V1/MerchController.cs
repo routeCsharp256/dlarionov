394515d4e11de67616c6cf5497705ce00981f51f
NotKohtpojiep <57583784+NotKohtpojiep@users.noreply.github.com>
2021-10-28 13:30:59 +0300

    #4 Complete tasks
    Removed unused classes

---
+++ src/OzonEdu.MerchandiseService.Api/Controllers/V1/MerchController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using OzonEdu.MerchandiseService.Api.Models;
+using OzonEdu.MerchandiseService.HttpModels;
+
+namespace OzonEdu.MerchandiseService.Api.Controllers.V1
+{
+    [ApiController]
+    [Route("v1/api/merch")]
+    [Produces("application/json")]
+    public class MerchController : ControllerBase
+    {
+        public MerchController()
+        {
+        }
+
+        [HttpGet("info/{id:int}")]
+        public async Task<ActionResult<MerchInfo>> GetInfoById(int id, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("issuance")]
+        public async Task<ActionResult> AddIssuance(MerchIssuanceViewModel merchIssuanceViewModel, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/OzonEdu.MerchandiseService.Api/Infrastructure/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace OzonEdu.MerchandiseService.Api.Infrastructure.Filters
+{
+    public class GlobalExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var resultObject = new
+            {
+                ExceptionType = context.Exception.GetType().FullName,
+                StackTrace = context.Exception.StackTrace
+            };
+
+            var jsonResult = new JsonResult(resultObject)
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+            context.Result = jsonResult;
+        }
+    }
+}
